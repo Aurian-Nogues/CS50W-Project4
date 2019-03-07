e7fb9f7234405dab6f964f4b89dffa1d8941c36f
Aurian-Nogues <aurian.nogues@gmail.com>
2019-03-07 23:46:40 +0000

    finished code

--- stocks/urls.py
+++ stocks/urls.py
@@ -8,9 +8,12 @@ urlpatterns = [
     path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
     path("create_account", views.createAccount, name="create_account"),
-    path('ideas', views.ideas, name='ideas'),
+    path('track_record', views.track_record, name='track_record'),
     path('trade', views.trade, name='trade'),
     path('build_trade/<str:ticker>/<str:name>', views.build_trade, name='build_trade'),
     path('close_trade/<str:ticker>/<str:open_price>/<str:open_date>', views.close_trade, name='close_trade'),
     path('record_trade', views.record_trade, name='record_trade'),
+    path('team_dashboard', views.team_dashboard, name='team_dashboard'),
+    path('team_track_record', views.team_track_record, name='team_track_record'),
+    
 ]
--- stocks/views.py
+++ stocks/views.py
@@ -8,6 +8,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse
 from .models import Trade_idea
 
+
 def index(request):
     #if user is not conntected send to login page
     if not request.user.is_authenticated:
@@ -31,6 +32,9 @@ def createAccount(request):
         if form.is_valid():
             form.save()
             # log the user in
+            username=form['username']
+            password=form['password1']
+            user = authenticate(request, username=username, password=password)
             return HttpResponseRedirect(reverse("index"))
     else:
         form = UserCreationForm()
@@ -68,7 +72,7 @@ def dashboard(request):
         current_price = float(data['price'])
         open_price = float(trade.open_price)
         target_price = float(trade.target_price)
-        upside = (current_price / target_price -1 ) * 100
+        upside = (target_price / current_price -1 ) * 100
         upside = str(round(upside, 2)) + '%'
         performance = (current_price / open_price -1 ) * 100
         performance = str(round(performance, 2)) + '%'
@@ -80,28 +84,86 @@ def dashboard(request):
         trade.save()
 
     trades = Trade_idea.objects.all().filter(user=user, status="open")
-
+    
     context = {
         "trades": trades,
     }
     return render(request, "stocks/dashboard.html", context)
 
-def ideas(request):
+def team_dashboard(request):
+    if not request.user.is_authenticated:
+        return render(request, "stocks/login.html", {"message": None})
+
+    #only execute if user is staff member, if not redirect to normal dashboard
+    if request.user.is_staff:
+        trades = Trade_idea.objects.all().filter(status="open")
+        #for each open trade 
+        for trade in trades:
+
+            #get the ticker and make a query to the api
+            ticker = trade.ticker
+            api_query = "https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=" + ticker + "&apikey=7ZON9TG94BAELGBM"
+            response = requests.get(api_query)
+            data = dict(response.json())
+            
+
+            #remove numbers from dictionnary so keys are accessible
+            data = data['Global Quote']
+            data = cut_off_numbers_from_dict_keys(data)
+
+            #get necessary data and calculate upside and performance. Convert them to a string with % to print nicely in html table
+            current_price = float(data['price'])
+            open_price = float(trade.open_price)
+            target_price = float(trade.target_price)
+            upside = (target_price / current_price -1 ) * 100
+            upside = str(round(upside, 2)) + '%'
+            performance = (current_price / open_price -1 ) * 100
+            performance = str(round(performance, 2)) + '%'
+
+            #write it in database
+            trade.current_price = current_price
+            trade.performance = performance
+            trade.upside = upside
+            trade.save()
+
+        trades = Trade_idea.objects.all().filter(status="open")
+
+        context = {"trades":trades
+
+        }   
+        return render(request, "stocks/team_dashboard.html", context)
+
+    return HttpResponseRedirect(reverse("dashboard"))
+
+
+
+def team_track_record(request):
+    if not request.user.is_authenticated:
+        return render(request, "stocks/login.html", {"message": None})
+
+    #only execute if user is staff member, if not redirect to normal dashboard
+    if request.user.is_staff:
+        trades = Trade_idea.objects.all().filter(status="closed")
+
+        context = {
+            "trades":trades,
+        }
+        return render(request, "stocks/team_track_record.html", context)
+
+    return HttpResponseRedirect(reverse("dashboard"))
+
+def track_record(request):
     #if user is not conntected send to login page
     if not request.user.is_authenticated:
         return render(request, "stocks/login.html", {"message": None})
 
+    user=request.user
+    trades = Trade_idea.objects.all().filter(user=user, status="closed")
+
     context = {
+        "trades":trades,
     }
-    return render(request, "stocks/ideas.html", context)
-
-def cut_off_numbers_from_dict_keys(dictionary):
-    result = {}
-    for key, val in dictionary.items():
-        new_key = key.split('. ')[1]
-        result[new_key] = val
-    return result
-
+    return render(request, "stocks/track_record.html", context)
 
 def trade(request):
     #if user is not conntected send to login page
@@ -195,12 +257,24 @@ def close_trade(request, ticker, open_price, open_date):
     
     user=request.user
 
-
-    #############################control for no matching trade
     trade = Trade_idea.objects.all().get(user=user, status="open", ticker=ticker, open_price=open_price, open_date=open_date)
-    print(trade)
+    close_price = float(trade.current_price)
+    open_price = float(trade.open_price)
+    performance = (close_price / open_price - 1) * 100 
+    performance = str(round(performance, 2)) + '%'
+
+    close_date = datetime.date.today()
     trade.status = "closed"
+    trade.close_price = close_price
+    trade.close_date = close_date
+    trade.performance = performance
     trade.save()
     return HttpResponseRedirect(reverse("dashboard"))
 
-# api key 7ZON9TG94BAELGBM
+def cut_off_numbers_from_dict_keys(dictionary):
+    result = {}
+    for key, val in dictionary.items():
+        new_key = key.split('. ')[1]
+        result[new_key] = val
+    return result
+
