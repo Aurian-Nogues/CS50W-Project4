d324bf1b2aa5888fcaa58c7e3f88ceced871dfa6
Aurian-Nogues <aurian.nogues@gmail.com>
2019-03-07 15:53:36 +0000

    wrote some test

--- stocks/tests.py
+++ stocks/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from .models import Trade_idea
-
+import requests, json
+from .views import cut_off_numbers_from_dict_keys
 
 
 # Create your tests here.
@@ -12,8 +13,46 @@ class StocksTestCase(TestCase):
             entry.save()
             entry = Trade_idea(user="George", ticker="GOOG", name="Alphabet", open_price=1150.53, message="I like it", open_date="2018-03-07", status="open")
             entry.save()
+        
     
     def test_new_ideas(self):
+        #test if new trade ideas can be added to database
         ideas = Trade_idea.objects.all()
         self.assertEqual(ideas.count(), 2)
+
+    def test_api_queries(self):
+        #make a request to api endpoint and check that length of returned dictionary hasn't changed. 
+
+        #https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=AAPL&apikey=7ZON9TG94BAELGBM
+        ticker = "AAPL"
+        api_query = "https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=" + ticker + "&apikey=7ZON9TG94BAELGBM"
+        response = requests.get(api_query)
+        data = dict(response.json())
+        expected = {'Global Quote': {'01. symbol': 'AAPL', '02. open': '173.8700', '03. high': '174.4400', '04. low': '172.0347', '05. price': '172.4300', '06. volume': '6506040', '07. latest trading day': '2019-03-07', '08. previous close': '174.5200', '09. change': '-2.0900', '10. change percent': '-1.1976%'}}
+        
+        assert len(data) == len(expected)
+
+    def test_cutoff_function(self):
+        #test if function to cut numbers from alphavantage response returns expected dict format
+        assert cut_off_numbers_from_dict_keys({
+            "1. symbol": "AAPL",
+            "2. name": "Apple Inc.",
+            "3. type": "Equity",
+            "4. region": "United States",
+            "5. marketOpen": "09:30",
+            "6. marketClose": "16:00",
+            "7. timezone": "UTC-05",
+            "8. currency": "USD",
+            "9. matchScore": "0.8889"
+        }) == {
+            "symbol": "AAPL",
+            "name": "Apple Inc.",
+            "type": "Equity",
+            "region": "United States",
+            "marketOpen": "09:30",
+            "marketClose": "16:00",
+            "timezone": "UTC-05",
+            "currency": "USD",
+            "matchScore": "0.8889"
+        }
 
